cfe4e53fba2053d2643b045c1cd6653c8bb41303
Jesus Yael Padron Grimaldo <jesusyaelpadrongrimaldo@gmail.com>
2025-06-13 19:28:19 -0600

    Cambios de nombre

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola, soy Jesus Yael Padron Grimaldo, del grupo GIDS5093
         </p>
         <a
           className="App-link"
